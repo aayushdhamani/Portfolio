aafe536aead6a061431a5f67109b983a09f205a7
Aayush dhamani <ayushdhamanibki@gmail.com>
2024-02-02 00:29:47 +0530

    contact

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,14 +14,14 @@ const Navbar: FC<NavbarProps> = ({}) => {
         <nav className="flex sticky shadow-md top-0 z-20 bg-body p-5 text-info justify-between">
             {/* <SearchBar /> */}
             <div>{/* Place holder for search bar */}</div>
-            <div className="flex items-center gap-5">
+            {/* <div className="flex items-center gap-5">
                 <Button  onClick={setFormVisibility}>
                     Contact Me <User size={18} />
                 </Button>
                 <button onClick={() => showMenuVisibility(!showMenu)} type="button">
                     <Menu className="flex lg:hidden cursor-pointer hover:text-primary transition-colors" />
                 </button>
-            </div>
+            </div> */}
         </nav>
     );
 };
